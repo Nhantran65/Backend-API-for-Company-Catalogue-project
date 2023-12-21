f53b8053f72fe88d3f87d6bacc7a0f3795fa23aa
Duc Thai <ducthai060501@gmail.com>
2023-12-21 14:56:20 +0200

    some fix merge fix

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,6 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    "./node_modules/flowbite-react/**/*.js",
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
@@ -73,5 +72,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require('flowbite/plugin')],
+  plugins: [require("tailwindcss-animate")],
 }
